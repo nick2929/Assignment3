359b94bbd0d5dfa352cd5f249d52fa1d0f2b0ca4
nick2929 <neilnikhil29@gmail.com>
2019-08-11 21:28:21 -0400

    First Commit

---
+++ StarwarsAPI/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using GraphQL.Client;
+using GraphQL.Common.Request;
+using GraphQL.Common.Response;
+
+
+namespace StarwarsAPI
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+                
+            //Graph QL Client 
+            var graphQlClient = new GraphQLClient("https://swapi.apis.guru/"); 
+
+            graphQlClient.DefaultRequestHeaders.Add("User-Agent", " GraphQLApp");
+
+            GraphQLRequest graphQLRequest = new GraphQLRequest
+            {
+                Query = "query{ allFilms { films { title director} }  }"
+            };
+
+            try
+            {
+                GraphQLResponse response = await graphQlClient.PostAsync(graphQLRequest);
+                List<Film> movies = new List<Film>();
+                foreach (Newtonsoft.Json.Linq.JObject f in response.Data.allFilms.films)
+                {
+                    Film film = f.ToObject<Film>();
+                    movies.Add(film);
+
+                }
+                film_list.ItemsSource = movies;
+
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine((string)ex.Message + " :: " + ex.InnerException.Message);
+            }
+
+            }
+    }
+}
---
+++ StarwarsAPI/Models/Film.cs
@@ -0,0 +1,8 @@
+﻿public class Film
+{
+    //Title of the movie
+    public string title { get; set; }
+    //Director of the movie
+    public string director { get; set; }
+
+}
